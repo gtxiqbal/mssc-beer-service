672d4555a8d0e481099b710c497e13b286f0226a
iqbal.alhusain <moh.iqbal.alhusain@gmail.com>
2020-11-14 10:56:51 +0700

    adding bootstrap - classes. Closes #7

---
+++ src/main/java/id/co/gtxiqbal/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,45 @@
+package id.co.gtxiqbal.msscbeerservice.bootstrap;
+
+import id.co.gtxiqbal.msscbeerservice.domain.Beer;
+import id.co.gtxiqbal.msscbeerservice.repository.BeerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    @Autowired
+    private BeerRepository beerRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if (beerRepository.count() == 0) {
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000001L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000002L)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+        }
+
+        System.out.println("Loaded Beers: " + beerRepository.count());
+    }
+}
